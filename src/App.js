42f4318f2c567ac23d36b402cda34c848b31122d
kosovmax <kosovmax@ukr.net>
2020-11-23 18:03:55 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import Statistics from './components/Statistics';
 import FriendList from './components/FriendList';
 import TransactionHistory from './components/TransactionHistory';
 
+import user from './json/user.json';
+import friends from './json/friends.json';
+import statisticalData from './json/statistical-data.json';
+import transactions from './json/transactions.json';
+
+import './base.css'
+
 
 export default function App(){
     return(
         <div>
-            <Profile />
-            <Statistics />
-            <FriendList />
-            <TransactionHistory />
+            <Profile
+                name={user.name}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+            />
+            <FriendList friends={friends} />
+            <Statistics title="Upload stats" stats={statisticalData} />
+            <Statistics stats={statisticalData} />
+            <TransactionHistory items={transactions} />
         </div>
     );
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,32 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 
-export default function Profile(){
+export default function Profile({name, tag, location, avatar, stats}){
     return (
-        <div class="profile">
-            <div class="description">
+        <div class={styles.profile}>
+            <div class={styles.description}>
                 <img
-                src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
+                src={avatar}
                 alt="Аватар пользователя"
-                class="avatar"
+                class={styles.avatar}
                 />
-                <p class="name">Petra Marica Kosovmax</p>
-                <p class="tag">@pmarica</p>
-                <p class="location">Salvador, Brasil</p>
+                <p class={styles.name}>{name}</p>
+                <p class={styles.tag}>{tag}</p>
+                <p class={styles.location}>{location}</p>
             </div>
 
-            <ul class="stats">
+            <ul class={styles.stats}>
                 <li>
-                <span class="label">Followers</span>
-                <span class="quantity">1000</span>
+                    <span class={styles.label}>Followers</span>
+                    <span class={styles.quantity}>{stats.followers}</span>
                 </li>
                 <li>
-                <span class="label">Views</span>
-                <span class="quantity">2000</span>
+                    <span class={styles.label}>Views</span>
+                    <span class={styles.quantity}>{stats.views}</span>
                 </li>
                 <li>
-                <span class="label">Likes</span>
-                <span class="quantity">3000</span>
+                    <span class={styles.label}>Likes</span>
+                    <span class={styles.quantity}>{stats.likes}</span>
                 </li>
             </ul>
         </div>
@@ -35,9 +35,21 @@ export default function Profile(){
 }
 
 Profile.defaultProps = {
-
+    name:"Petra Marica",
+    tag:"@pmarica",
+    location:"Salvador, Brasil",
+    avatar:"https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg",
+    stats:{
+        "followers": 0,
+        "views": 0,
+        "likes": 0
+      }
 };
 
 Profile.propTypes = {
-
+    name:PropTypes.string.isRequired,
+    tag:PropTypes.string.isRequired,
+    location:PropTypes.string.isRequired,
+    avatar:PropTypes.string.isRequired,
+    status:PropTypes.object.isRequired,
 }
